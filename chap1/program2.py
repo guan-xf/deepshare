00555b4da55f098e89e6fd3ae5acfb40842b0cf8
guan-xf <guanxf7611@163.com>
2019-08-06 20:56:49 +0800

    update login

--- chap1/program2.py
+++ chap1/program2.py
@@ -19,6 +19,7 @@ with open('account', 'r+', encoding='UTF-8') as fa, open('blacklist', 'a+', enco
                 password_lo = input('请输入密码：').strip()
                 if password_lo == user_info[user_lo][0]:
                     print('登录成功')
+                    return user_lo
                     break
                 else:
                     print('密码输入错误！')
@@ -71,5 +72,69 @@ with open('account', 'r+', encoding='UTF-8') as fa, open('blacklist', 'a+', enco
                 except ValueError:
                     print('薪资输入错误')
 
+    def do_shopping(user):
+        goods_dic = {'apple': 10, 'mac': 10000, 'iphone': 8000, 'lenovo': 30000, 'chicken': 10}
+        shopping = []
+        goods_list = []
+        print('商品信息'.center(18, '*'))
+        print('商品' + '\t' * 4 + '价格')
+        for goods, value in goods_dic.items():
+            print(goods.ljust(10, '-') + str(value).rjust(10, '-'))
+        print("*" * 20)
+        while True:
+            choice = input('请输入需要够买的商品：').strip()
+            try:
+                number = int(input('请输入商品购买数量：'))
+            except ValueError:
+                number = int(input('输入有误，请重新输入数量：'))
+            if choice not in goods_dic.keys():
+                print('未找到对应商品，请重新输入')
+                continue
+            else:
+                cast = goods_dic[choice] * number
+                if cast <= float(user_info[user][1]):
+                    print('购买成功！')
+                    float(user_info[user][1]) - cast
+                    user_info[user][1] = str(float(user_info[user][1]) - cast)
+                    with open('account', 'r', encoding='UTF-8') as f:
+                        lines = f.readlines()
+                    with open('account', 'w', encoding='UTF-8') as f_new:
+                        for each in lines:
+                            if each.startswith(user):
+                                f_new.write(user + '|' + user_info[user][0] + '|' + user_info[user][1])
+                                continue
+                            f_new.write(each)
+                else:
+                    if_charge = input('账户余额不足!是否充值？y/n')
+                    if if_charge == 'y':
+                        charge = input('请输入要充值的金额：')
+                        try:
+                            if '.' in charge:
+                                user_info[user][1] = str(float(charge) + float(user_info[user][1]))
+
+                            else:
+                                user_info[user][1] = str(int(charge) + int(user_info[user][1]))
+
+                            with open('account', 'r', encoding='UTF-8') as f:
+                                lines = f.readlines()
+                            with open('account', 'w', encoding='UTF-8') as f_new:
+                                for each in lines:
+                                    if each.startswith(user):
+                                        f_new.write(user + '|' + user_info[user][0] + '|' + user_info[user][1])
+                                        continue
+                                    f_new.write(each)
+                        except ValueError:
+                            print('薪资输入错误')
+
+            if_quit = input('是否继续购买其他商品？Y/N').upper()
+            if if_quit == 'N':
+                break
+            elif if_quit != 'Y':
+                print('输入错误，程序退出')
+                break
+
+
     if __name__ == '__main__':
-        login()
+
+        do_shopping(login())
+
